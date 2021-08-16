6ac70f78a84b9b9196cc144932d64341700263e5
DavidPortador <david.raw172@gmail.com>
2021-08-15 21:33:11 -0500

    Planeacion de temas

---
+++ 1. Fundamentos/Diagramas/suma.psc
@@ -0,0 +1,6 @@
+Algoritmo suma
+	a <- 4
+	b <- 3
+	c <- a+b
+	Escribir c
+FinAlgoritmo
---
+++ Recursos/suma.java
@@ -0,0 +1,9 @@
+class suma{
+	public static void main(String[] args) {		// 0. Estructura
+		int a, b, c; 								// 1. Variables.
+		a = 4;										// 2. Inicialización
+		b = 3;										//    de variables.
+		c = a + b;									// 3. Operación
+		System.out.println(c);						// 4. Salida
+	}
+}
